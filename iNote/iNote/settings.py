4bf51dfdf5d122bae225910b28640055df352635
idarbandi <darbandidr99@gmail.com>
2023-04-15 22:08:24 +0330

    integrated Django with React

--- iNote/iNote/settings.py
+++ iNote/iNote/settings.py
@@ -35,7 +35,9 @@ ROOT_URLCONF = "iNote.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "frontend/build"    
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -97,6 +99,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend/build/static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
