221e0b9246e9a15805da0901013bba2300367f4b
idarbandi <darbandidr99@gmail.com>
2023-04-16 12:51:57 +0330

    a few hot fixes

--- iNote/frontend/src/App.js
+++ iNote/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes as Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header'
 import NotelistsPage from './pages/NotelistsPage'
